6a0cc7fce07c33d39ce41c07de001b5e6361dcb9
TusharDC <tushar@digitalcappuccino.com>
2024-04-15 12:35:02 +0530

    map location updated

--- src/components/common/Map.js
+++ src/components/common/Map.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Map = () => {
-  return (
-    <div className='py-20 container'>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3507.0993844349114!2d77.06760997528303!3d28.47655347575029!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d198a5e52816d%3A0xf6f71ef0ba1164e3!2sDiego%20Club!5e0!3m2!1sen!2sin!4v1706083817342!5m2!1sen!2sin" className='w-full' height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    </div>
-  )
-}
+    return (
+        <div className="container py-20">
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3507.3252039077684!2d77.06029887549484!3d28.469751375754036!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjjCsDI4JzExLjEiTiA3N8KwMDMnNDYuNCJF!5e0!3m2!1sen!2sin!4v1713164378672!5m2!1sen!2sin"
+                className="w-full"
+                height="450"
+                allowfullscreen=""
+                loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"
+            ></iframe>
+        </div>
+    );
+};
 
-export default Map
+export default Map;
